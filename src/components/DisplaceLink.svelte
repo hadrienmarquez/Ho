349432f3f548a357b604dbf82728ab42fe9b0d35
Hadrien Marquez <hadrienmarquez@gmail.com>
2022-12-13 23:58:54 +0100

    Refactoring links with displace animation on hover into a component

---
+++ src/components/DisplaceLink.svelte
@@ -0,0 +1,9 @@
+<script>
+    let cssClass;
+    export { cssClass as class };
+    export let url;
+    export let after_content;
+</script>
+
+
+<a href={url} {after_content} class="{cssClass} after:content-[attr(after\_content)] font-maragsa z-10 relative after:-z-10 after:absolute after:w-full after:pointer-events-none after:duration-300 after:opacity-0 after:top-0 after:left-0 after:transition-all after:ease-out  hover:after:left-1 hover:after:opacity-100"><slot/></a>

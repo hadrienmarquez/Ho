670efd2f6ecedddcf573a3acfb86890708245b4c
Hadrien Marquez <hadrienmarquez@gmail.com>
2022-12-14 00:00:34 +0100

    Refactoring animated links, adding transitions and changing mobile look

--- src/components/BurgerMenu.svelte
+++ src/components/BurgerMenu.svelte
@@ -1,53 +1,84 @@
 <script lang="ts">
-    import { fade } from 'svelte/transition';
+    import { fade, scale } from "svelte/transition";
+    import DisplaceLink from "./DisplaceLink.svelte";
     let visible = false;
+
+    const links = [
+        {
+            url: "/a-propos",
+            text: "À propos",
+        },
+        {
+            url: "/journal-de-bord",
+            text: "Journal de bord",
+        },
+        {
+            url: "/contact",
+            text: "Contact",
+        },
+    ];
 
     function handleClick() {
         visible = !visible;
-        console.log(visible)
     }
 </script>
 
-<div class="group my-auto border border-brown-100 bg-red-700 relative after:w-full after:h-full after:bg-brown-400 after:-z-10 after:absolute after:top-1 after:left-1 after:transition-all after:ease-out after:duration-300  hover:after:left-0 hover:after:top-0">
+<div
+    class="group my-auto border border-brown-100 bg-red-700 relative after:w-full after:h-full after:bg-brown-400 after:-z-10 after:absolute after:top-1 after:left-1 after:transition-all after:ease-out after:duration-300  hover:after:left-0 hover:after:top-0"
+>
     <button
         on:click={handleClick}
-        class="flex md:space-x-2 p-4 md:px-4 md:py-2 z-10 after:z-0 after:absolute after:w-full after:h-full after:bg-brown-300 after:top-0 after:left-0 after:scale-y-0 after:transition-transform after:duration-300 after:ease-out after:origin-bottom-left group-hover:after:scale-100 after:delay-75"
+        class="flex z-10 p-4 md:space-x-2 md:px-4 md:py-2 after:z-0 after:absolute after:w-full after:h-full after:bg-brown-300 after:top-0 after:left-0 after:scale-y-0 after:transition-transform after:duration-300 after:ease-out after:origin-bottom-left group-hover:after:scale-100 after:delay-75"
     >
-        <p class="hidden md:block my-auto text-sm font-bold text-brown-100 z-10 duration-300 group-hover:text-brown-800">Menu</p>
+        <p
+            class="hidden md:block my-auto text-sm font-bold text-brown-100 z-10 duration-300 group-hover:text-brown-800"
+        >
+            Menu
+        </p>
         <div class="my-auto space-y-1 z-10">
-            <span class="block w-8 h-px bg-brown-100 duration-300 group-hover:bg-brown-800" />
-            <span class="block w-8 h-px bg-brown-100 duration-300 group-hover:bg-brown-800" />
-            <span class="block w-8 h-px bg-brown-100 duration-300 group-hover:bg-brown-800" />
+            <span
+                class="block w-8 h-px bg-brown-100 duration-300 group-hover:bg-brown-800"
+            />
+            <span
+                class="block w-8 h-px bg-brown-100 duration-300 group-hover:bg-brown-800"
+            />
+            <span
+                class="block w-8 h-px bg-brown-100 duration-300 group-hover:bg-brown-800"
+            />
         </div>
     </button>
 </div>
 
-
-
-
 <!-- Popup container class -->
 {#if visible}
-<div in:fade="{{duration: 150}}" out:fade="{{duration: 200}}" class="flex items-start justify-end fixed top-0 left-0 w-full h-full bg-yellow-300 bg-opacity-80 z-50" >
-    <div class="bg-green-700 rounded-full w-[250px] h-[250px] m-2 md:w-[500px] md:h-[500px]">
-        <div class="flex flex-col items-center justify-around h-full">
-            <div class="flex flex-col justify-between items-center space-y-2 text-3xl mt-10 md:text-6xl md:space-y-10 md:mb-7 h-fit">
-                <a href="/a-propos" class="text-brown-100 font-maragsa z-10  
-                relative  after:content-['À\00a0propos']  after:-z-10
-                 after:text-red-400
-                 after:absolute after:pointer-events-none after:duration-300 after:opacity-0 after:top-0 after:left-0 after:transition-all after:ease-in-out  hover:after:opacity-100 hover:after:left-1">À&nbsp;propos</a>
-                <a href="/journal-de-bord" class="text-brown-100 font-maragsa z-10  
-                relative after:content-['Journal\0000a0de\0000a0bord']  after:-z-10
-                 after:text-red-400
-                 after:absolute after:pointer-events-none after:duration-300 after:opacity-0 after:top-0 after:left-0 after:transition-all after:ease-in-out  hover:after:opacity-100 hover:after:left-1">Journal&nbsp;de&nbsp;bord</a>
-                <a href="/contact" class="text-brown-100 font-maragsa z-10  
-                relative  after:content-['Contact']  after:-z-10
-                 after:text-red-400
-                 after:absolute after:pointer-events-none after:duration-300 after:opacity-0 after:top-0 after:left-0 after:transition-all after:ease-in-out  hover:after:opacity-100 hover:after:left-1">Contact</a>
+    <div
+        in:fade={{ duration: 500 }}
+        out:fade={{ duration: 500, delay: 300 }}
+        class="flex items-start justify-end fixed top-0 left-0 w-full h-full bg-yellow-300 bg-opacity-80 z-50 sm:justify-center sm:items-center"
+    >
+        <div
+            in:scale={{ duration: 500, delay: 200 }}
+            out:scale={{ duration: 500 }}
+            class=" flex flex-col items-start justify-around bg-green-700 rounded-3xl w-fit m-2 px-10 h-[28rem] sm:items-center sm:rounded-full sm:p-0 sm:m-0 sm:w-[400px] sm:h-[400px] md:w-[500px] md:h-[500px]"
+        >
+            <div
+                class="flex flex-col justify-between items-start space-y-4 text-4xl h-fit sm:mt-[5rem] sm:items-center sm:space-y-8 sm:mb-7 sm:text-5xl  md:text-6xl md:mt-[6rem] md:space-y-10"
+            >
+                {#each links as link}
+                    <DisplaceLink
+                        class="text-brown-100 after:text-red-400"
+                        after_content={link.text}
+                        url={link.url}>{link.text}</DisplaceLink
+                    >
+                    <span class="w-full sm:hidden border-b border-red-300" />
+                {/each}
             </div>
-            <button on:click={handleClick} class="relative text-xl text-center h-8 w-8 md:h-12 md:w-12 md:text-3xl font-bold text-brown-100 z-10 font-maragsa after:rounded-full after:absolute after:-z-10 after:scale-0 after:top-0 after:-left-0 after:w-full after:h-full after:bg-red-400 hover:after:scale-150 after:transition-transform">X</button>
+            <button
+                on:click={handleClick}
+                class="relative text-brown-100 z-10 font-maragsa  border-red-400 border rounded-full text-2xl text-center self-end h-10 w-10 sm:h-8 sm:w-8 sm:self-center  sm:border-0  md:h-12 md:w-12 md:text-3xl md:font-bold after:rounded-full after:absolute after:-z-10 after:scale-0 after:top-0 after:-left-0 after:w-full after:h-full after:bg-red-400 hover:after:scale-150 after:transition-transform"
+                >X</button
+            >
         </div>
+        <!-- Popup Menu -->
     </div>
-    <!-- Popup Menu -->
-
-</div>
 {/if}
